a79480e3a87e9be4761034b12577a69dfb61d705
PedroDeBos <pedrodebos@gmail.com>
2022-05-21 11:40:33 +0200

    Made it so the list doesn't get infinitely made, succesfully fixed the vignette

--- R/pokemon_type.R
+++ R/pokemon_type.R
@@ -8,13 +8,13 @@
 #' @examples pokemon_type("Chimchar", "Drapion")
 pokemon_type<-function(...){
   arguments<-list(...) #Storing all arguments as a list
+  all_pokemon<-list() #Preparing a list for the for-do-done loop
   for (x in arguments) #Making a command which repeats for all arguments in the list
   { if (is.na(pokemon$type2[pokemon$name==x])) #Determening whether the pokemon has 1 or 2 types, giving the appropriate message based on this
     output<-(paste0(pokemon$name[pokemon$name==x], " is a ", pokemon$type1[pokemon$name==x], " type"))
     else {
     output<-(paste0(pokemon$name[pokemon$name==x], " is a ", pokemon$type1[pokemon$name==x], " and a ", pokemon$type2[pokemon$name==x], " type"))
     }
-  all_pokemon<-list()
   all_pokemon[[x]]<-output
   }
   print(all_pokemon)
